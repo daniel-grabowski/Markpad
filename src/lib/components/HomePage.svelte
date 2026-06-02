30a79db1dc241af61616eccdbb033b43fa013742
dayeggpi <hycday@gmail.com>
2026-06-02 09:34:36 +0200

    Update HomePage.svelte

--- src/lib/components/HomePage.svelte
+++ src/lib/components/HomePage.svelte
@@ -58,7 +58,6 @@
 		</button>
 	</div>
 
-	{#if settings.showRecentFiles}
 	<div class="recent-section">
 		<h3>{t('home.recentFiles', settings.language)}</h3>
 		{#if recentFiles.length > 0}
@@ -109,7 +108,6 @@
 			<p class="empty-recent">{t('home.noRecentFiles', settings.language)}</p>
 		{/if}
 	</div>
-	{/if}
 </div>
 <div class="version-tag">v{version}</div>
 
